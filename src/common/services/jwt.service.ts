4720790a9f77be88be253b7247851a8f9b6ea075
amir <mobina.mahdi2014@gmail.com>
2025-12-09 21:35:54 +0330

    Update jwt.service.ts

--- src/common/services/jwt.service.ts
+++ src/common/services/jwt.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import jwt, { SignOptions, JwtPayload } from 'jsonwebtoken';
+import { InternalServerException } from '../exceptions';
+import { ErrorCode } from '../enums';
 
 @Injectable()
 export class CustomJwtService {
@@ -10,7 +12,7 @@ export class CustomJwtService {
     this.JWT_SECRET = this.configService.get<string>('JWT_SECRET')!;
 
     if (!this.JWT_SECRET) {
-      throw new Error('JWT_SECRET is not defined in environment variables!');
+      throw new InternalServerException('JWT_SECRET is not defined in environment variables!', ErrorCode.INTERNAL_ERROR);
     }
   }
   async sign(userId: number, options?: SignOptions): Promise<string> {
